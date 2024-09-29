d5dfbba1a4162f53e886c6f20095e06b63e5703f
Kellyanne Smith <greekgeek741@icloud.com>
2024-09-29 17:18:45 -0500

    Initial commit

---
+++ src/main/java/net/c4fey/astral_alchemy/AstralAlchemy.java
@@ -0,0 +1,16 @@
+package net.c4fey.astral_alchemy;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AstralAlchemy implements ModInitializer {
+	public static final String MOD_ID = "astral_alchemy";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/c4fey/astral_alchemy/AstralAlchemyClient.java
@@ -0,0 +1,10 @@
+package net.c4fey.astral_alchemy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AstralAlchemyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
